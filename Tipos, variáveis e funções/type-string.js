18c98ee30fcb4c78a1c819f97a56d8b1fa2c3430
Paulo Ricardo <paulo_tolkien@outlook.com>
2022-12-20 21:33:08 -0300

    finally lesson of types

--- Tipos, variáveis e funções/type-string.js
+++ Tipos, variáveis e funções/type-string.js
@@ -8,3 +8,8 @@ const stringNumber = "188273";
 
 const textSimple = 'Paulo says: "Hi!"';
 console.log(textSimple);
+
+//some methods to practice manipulation of strings
+console.log(firstString.toLowerCase()); // all letters are lower case
+console.log(secondString.replace('o', '0')); // Replace a letter to other.
+console.log(password.length);
---
+++ type-boolean.js
@@ -0,0 +1,7 @@
+const firstNumber = 10;
+const secondNumber = 3;
+
+console.log(firstNumber === secondNumber);
+
+let validation = firstNumber === secondNumber;
+console.log(validation);
