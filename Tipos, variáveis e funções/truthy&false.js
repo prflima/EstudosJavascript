c15d138e9213de5abc2dcb4beb3dee426823547d
Paulo Ricardo <paulo_tolkien@outlook.com>
2023-01-31 23:36:10 -0300

    truthy e falsy

---
+++ Tipos, variáveis e funções/truthy&false.js
@@ -0,0 +1,12 @@
+// Boolean 
+var hasLoggedUser = true;
+var hasProductsInWallet = false;
+
+// Truthy or falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == hasProductsInWallet) // => true
+console.log("" == hasProductsInWallet) // => true
+console.log(1 == hasLoggedUser) // => true
