146f3eba7f32a56cd937884ac63e201d19beb160
Paulo Ricardo <paulo_tolkien@outlook.com>
2023-02-07 23:45:29 -0300

    dynamic types

---
+++ Tipos, variáveis e funções/dynamicType.js
@@ -0,0 +1,15 @@
+/* O javascript é uma linguagem dinâmica e interpretada. Dinâmica porque não precisamos dizer o tipo de dado 
+que a variável vai receber, ela se adapta conforme a maneira e o tipo do dado que estamos atribuindo.
+E é interpretada pois não precisa ser compilada para ser executada.*/
+
+/*JavaScript is a dynamic, interpreted language. Dynamic because we don't need to say the type of data
+that the variable will receive, it adapts according to the way and type of data that we are assigning.
+And it's interpreted because it doesn't need to be compiled to run.*/
+
+
+//Dynamic type: 
+let example = "Isso é uma string / This is a string";
+console.log(typeof(example));
+
+example = 123 // Agora é um número / Now it's a number
+console.log(typeof(example));
